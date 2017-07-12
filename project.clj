3558fbbefbc7dd5de057d7808b56af396223c83d
보롱킴 <dsdgun@gmail.com>
2017-07-12 23:14:12 +0900

    [polishing] add cider-nrepl 0.15.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/dsdstudio/clj-mwb-exporter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]])
