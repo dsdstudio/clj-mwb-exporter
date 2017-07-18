462f0b613e5dea7872728585ced21e61298d3e16
보롱킴 <dsdgun@gmail.com>
2017-07-18 22:55:22 +0900

    [hotfix] clojure version 1.8 -> 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/dsdstudio/clj-mwb-exporter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
   :main clj-mwb-extractor.core
   :aot [clj-mwb-extractor.core]
   :jar true
