f1ae871474a3e52d259841806b403cc6e56088cc
보롱킴 <dsdgun@gmail.com>
2017-07-21 23:55:10 +0900

    [hotfix] adding create sql syntax

--- test/clj_mwb_extractor/core_test.clj
+++ test/clj_mwb_extractor/core_test.clj
@@ -3,13 +3,19 @@
             [clojure.pprint :refer [pprint]]
             [clj-mwb-extractor.core :refer :all]))
 
+(defn write-sql [table]
+  (let [table-name (:name table)]
+    (str "CREATE TABLE " (:schema-name table) "." table-name)))
+
 (deftest test0
   (testing "테이블의 foreignkey데이터 확인"
-    (let [r (doall (get-mwb-dsl "resources/test.mwb"))]
-      (pprint (->> r
-                   first
-                   :tables
-                   (filter #(= "hostel_reserv" (get % :name)))))
-      (is (= 1 1))))
+    (let [r (doall (get-mwb-dsl "resources/test.mwb"))
+          table (->> r
+                     first
+                     :tables
+                     first)]
+      (pprint (keys table))
+      (pprint (write-sql table))
+      (is (= 1 1)))
   (testing "test2"
-    (is (= 1 1))))
+    (is (= 1 1)))))
